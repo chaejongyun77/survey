de910a86c27505d29e09354e99dd049683009d2b
chaejongyun77 <140371161+chaejongyun77@users.noreply.github.com>
2026-04-05 19:58:21 +0900

    feat : redis 설정
    임베디드 redis -> redis cli사용

---
+++ src/main/java/com/woongjin/survey/global/config/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.woongjin.survey.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis 공통 설정
+ * - Key/Value를 String으로 직렬화
+ * - redis-cli에서 확인할 때 사람이 읽을 수 있는 형태로 저장
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
