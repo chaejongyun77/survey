82fc25e6c07c3ee4755f0edb5ef6c737c4e1964c
chaejongyun77 <140371161+chaejongyun77@users.noreply.github.com>
2026-04-08 23:30:03 +0900

    feat : Tailwind CSS login페이지 추가

---
+++ src/main/resources/static/js/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FF6600',
+                'primary-hover': '#E55F00',
+            },
+            fontFamily: {
+                sans: ['Noto Sans KR', 'sans-serif'],
+            }
+        }
+    }
+}
